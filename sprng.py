06582d7a395cce4d5986fe10a846367adaf77f59
syreal@moonshadow <luke@ltsyrealjones.com>
2020-12-04 17:05:23 -0500

    Add scene callback

--- sprng.py
+++ sprng.py
@@ -2,21 +2,34 @@ import arena
 
 
 
-arena.init("arena.andrew.cmu.edu", "realm", "sprng-changeme")
+def prnum_d6_callback(msg):
+  print("d6_callback: ", msg)
+  
+  #TODO: get new random number between 1 - 6
+  
+  #TODO: update prnum-render with new random number
+
+arena.init(
+  "arena.andrew.cmu.edu",
+  "realm",
+  "sprng-changeme",
+  prnum_d6_callback
+)
 
 prnum = arena.Object(
-    objName="prnum-render",
-    objType=arena.Shape.text,
-    color=(255, 255, 255),
-    location=(0, 50, -141),
-    scale=(100, 100, 100),
-    text="6",
+  objName="a-prnum",
+  objType=arena.Shape.text,
+  color=(255, 255, 255),
+  location=(0, 50, -141),
+  scale=(100, 100, 100),
+  text="6",
+  clickable=True,
 )
 
 
 
-input("Press any key to quit SPRNG...")
+#input("Press any key to quit SPRNG...")
 
-prnum.delete()
+#prnum.delete()
 
 arena.handle_events()
