09176573a2afe1df460c7cad5053eaf987d54dde
syreal@moonshadow <luke@ltsyrealjones.com>
2020-12-04 16:28:37 -0500

    Make number more easily visble

--- sprng.py
+++ sprng.py
@@ -5,9 +5,11 @@ import arena
 arena.init("arena.andrew.cmu.edu", "realm", "sprng-changeme")
 
 prnum = arena.Object(
+    objName="prnum-render",
     objType=arena.Shape.text,
     color=(255, 255, 255),
-    location=(0, 2, 0),
+    location=(0, 50, -141),
+    scale=(100, 100, 100),
     text="6",
 )
 
