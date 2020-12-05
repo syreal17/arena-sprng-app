0729c134f9e2f7ab4f39ee649683729471fcf6b8
syreal@moonshadow <luke@ltsyrealjones.com>
2020-12-05 09:35:21 -0500

    Add cube as functional button for d6

--- sprng.py
+++ sprng.py
@@ -1,48 +1,72 @@
-#NOTE: this will come in handy soon! (from gh/ARENA-py/examples/core.py)
-## trick: obtain an arena.py Object for an already-existing global scene object named "cameraRig"
-## in order to update it's data attributes
-#rig = arena.Object(objName="cameraRig")
-#rig.update(
-#    data='{"animation": {"property": "position", "to": "0 10 20", "easing": "linear", "dur": 1000}}')
-#input("")
-#rig.update(
-#    data='{"animation": {"property": "position", "to": "0 10 -200", "easing": "linear", "dur": 1000}}')
-
-
-
 import arena
+import json
+import random
 
 
 
-def prnum_d6_callback(msg):
-  print("d6_callback: ", msg)
+def scene_callback(msg):
+  #print("scene_callback: -->\n", msg)
+
+  jsonMsg = json.loads(msg)
+
+  # Detect user click for new num and roll again
+  if jsonMsg["object_id"] == "a-die0"\
+  and jsonMsg["type"] == "mousedown":
+    d6Roll = gen_d6_num()
+    update_prnum_text(str(d6Roll))
+    # TODO : log timestamp, user and roll here
+
+
+def gen_d6_num():
+  #print("[DEBUG] gen_d6_num: here!")
   
-  #TODO: get new random number between 1 - 6
+  # Get new random number between 1 - 6
+  return random.randint(1, 6)
+
+
+def update_prnum_text(newText):
+  #print("[DEBUG] update_prnum_text: here!")
   
-  #TODO: update prnum-render with new random number
+  # Update prnum-render with new random number
+  prnum.update(text=newText)
+
+
 
 arena.init(
   "arena.andrew.cmu.edu",
   "realm",
   "sprng-changeme",
-  prnum_d6_callback
+  scene_callback
+)
+
+
+die0 = arena.Object(
+  objName="a-die0",
+  objType=arena.Shape.cube,
+  color=(255, 255, 255),
+  location=(0, 50, -140),
+  scale=(25, 25, 25),
+  clickable=True,
+  persist=True,
+)
+
+lightDie = arena.Object(
+  objName="a-lightDie",
+  objType=arena.Shape.light,
+  location=(0, 50, -115),
+  persist=True,
 )
 
 prnum = arena.Object(
   objName="a-prnum",
-  objType=arena.Shape.cube,
-  color=(255, 255, 255),
-  location=(0, 50, -141),
+  objType=arena.Shape.text,
+  color=(0, 0, 0),
+  location=(0, 50, -125),
   scale=(100, 100, 100),
-  #text="6",
-  #persist=True,
-  clickable=True,
+  text="6",
+  persist=True,
 )
 
 
-
-#input("Press any key to quit SPRNG...")
-
-#prnum.delete()
 
 arena.handle_events()
