1e91a5895490eee2d869bc7a732207324a655d76
syreal@moonshadow <luke@ltsyrealjones.com>
2020-12-05 07:19:40 -0500

    Add script to delete prnum

---
+++ clear-a-prnum.py
@@ -0,0 +1,11 @@
+import arena
+
+
+
+arena.init(
+  "arena.andrew.cmu.edu",
+  "realm",
+  "sprng-changeme"
+)
+prnum = arena.Object(objName="a-prnum")
+prnum.delete()
--- sprng.py
+++ sprng.py
@@ -1,3 +1,15 @@
+#NOTE: this will come in handy soon! (from gh/ARENA-py/examples/core.py)
+## trick: obtain an arena.py Object for an already-existing global scene object named "cameraRig"
+## in order to update it's data attributes
+#rig = arena.Object(objName="cameraRig")
+#rig.update(
+#    data='{"animation": {"property": "position", "to": "0 10 20", "easing": "linear", "dur": 1000}}')
+#input("")
+#rig.update(
+#    data='{"animation": {"property": "position", "to": "0 10 -200", "easing": "linear", "dur": 1000}}')
+
+
+
 import arena
 
 
@@ -18,11 +30,12 @@ arena.init(
 
 prnum = arena.Object(
   objName="a-prnum",
-  objType=arena.Shape.text,
+  objType=arena.Shape.cube,
   color=(255, 255, 255),
   location=(0, 50, -141),
   scale=(100, 100, 100),
-  text="6",
+  #text="6",
+  #persist=True,
   clickable=True,
 )
 
