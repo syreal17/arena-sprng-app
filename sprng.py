40b076c724a5bd5710547ab103bf11620a5b2dc2
syreal17 <ltjones@acm.org>
2021-01-11 17:00:54 -0500

    Apply color workaround

--- sprng.py
+++ sprng.py
@@ -6,26 +6,29 @@ from arena import *
 
 
 
-# DEFs -----------------------------------------------------------------------
+# CALLBACKS -------------------------------------------------------------------
 
 def scene_callback(msg):
-  print("DEBUG: -1")
+  #DEBUG:OFF#print("DEBUG: -1")
   jsonMsg = msg
 
-  print("DEBUG: 0")
+  #DEBUG:OFF#print("DEBUG: 0")
   # Detect user click for new num and roll again -->
   if jsonMsg["object_id"] == "a-die1"\
   and jsonMsg["type"] == "mousedown":
-    print("DEBUG: 1")
+    #DEBUG:OFF#print("DEBUG: 1")
     d6Roll = gen_d6_num()
-    print("DEBUG: 2")
+    #DEBUG:OFF#print("DEBUG: 2")
     update_die1_text(str(d6Roll))
-    print("DEBUG: 3")
+    #DEBUG:OFF#print("DEBUG: 3")
     # Log timestamp, user and roll -->
     print(jsonMsg["timestamp"] + ": " + \
           jsonMsg["data"]["source"] + " rolled a " + str(d6Roll))
-    print("DEBUG: 4")
+    #DEBUG:OFF#print("DEBUG: 4")
 
+
+
+# OTHER DEFs ------------------------------------------------------------------
 
 def gen_d6_num():
   # Get new random number between 1 - 6
@@ -37,15 +40,18 @@ def update_die1_text(newText):
   die1_text.update(text=newText)
 
 
+
+# MAIN ------------------------------------------------------------------------
+
 arena = Arena( "arena.andrew.cmu.edu",
                "realm",
                "sprng-changeme-0.1.0",
                on_msg_callback=scene_callback )
-# MAIN -----------------------------------------------------------------------
+
 @arena.run_once # make this function a task that runs once at startup
 def main():
     die1 = Cube( object_id="a-die1",
-                 color=Color(255, 255, 255),
+                 color=(255, 255, 255),
                  position=Position(0, 50, -140),
                  scale=Scale(25, 25, 25),
                  clickable=True,
@@ -60,14 +66,16 @@ def main():
 
 
     die1_text = Text( object_id="a-die1_text",
-                      color=Color(0, 0, 0),
+                      color=(0, 0, 0),
                       position=Position(0, 50, -125),
                       scale=Scale(100, 100, 100),
-                      text="6",
+                      text="i",
                       persist=True )
     arena.add_object(die1_text)
 
 
 
-# start tasks
+# THREADS ---------------------------------------------------------------------
+
 arena.run_tasks()
+
