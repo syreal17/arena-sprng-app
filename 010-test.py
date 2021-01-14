514b35719a00f74a70f92c4d488041c474d61ab0
syreal17 <ltjones@acm.org>
2021-01-14 04:32:48 -0500

    Make arena objects more conventional globals

---
+++ 010-test.py
@@ -0,0 +1,15 @@
+from arena import *
+
+# create library
+arena = Arena("arena.andrew.cmu.edu", "realm", "sprng-changeme-2")
+
+@arena.run_once # make this function a task that runs once at startup
+def main():
+    # make a cube
+    cube = Cube(object_id="myCube", position=(0,3,-3), scale=(2,2,2))
+
+    # add the cube to ARENA
+    arena.add_object(cube)
+
+# start tasks
+arena.run_tasks()
--- sprng.py
+++ sprng.py
@@ -6,11 +6,28 @@ from arena import *
 
 
 
+# GLOBALS 1/2 -----------------------------------------------------------------
+
+arena = Arena( "arena.andrew.cmu.edu",
+               "realm",
+               "sprng-changeme-2" )
+
+dice_light = Light( object_id="a-dice_light",
+                    position=Position(0, 50, -115),
+                    persist=True )
+
+die1_text = Text( object_id="a-die1_text",
+                  color=Color(0, 0, 0),
+                  position=Position(0, 50, -125),
+                  scale=Scale(100, 100, 100),
+                  text="i",
+                  persist=True )
+
+
+
 # HANDLERS --------------------------------------------------------------------
 
 def die_click_handler(evt):
-    global arena
-    global die1_text
 
     print("in die click handler")
     
@@ -33,6 +50,11 @@ def die_click_handler(evt):
 
 
 
+# GLOBALS 2/2 -----------------------------------------------------------------
+# TODO : delete this section or move the die1 dec back here
+
+
+
 # OTHER DEFs ------------------------------------------------------------------
 
 def gen_d6_num():
@@ -41,23 +63,13 @@ def gen_d6_num():
 
 
 
-# GLOBALS ---------------------------------------------------------------------
-
-arena = None
-die1_text = None
-
-
-
 # MAIN ------------------------------------------------------------------------
-
-arena = Arena( "arena.andrew.cmu.edu",
-               "realm",
-               "sprng-changeme-0.1.0" )
 
 @arena.run_once # make this function a task that runs once at startup
 def main():
+    print("here main1")
     die1 = Cube( object_id="a-die1",
-                 color=(255, 255, 255),
+                 color=Color(255, 255, 255),
                  position=Position(0, 50, -140),
                  scale=Scale(25, 25, 25),
                  clickable=True,
@@ -65,20 +77,7 @@ def main():
                  evt_handler=die_click_handler )
     arena.add_object(die1)
 
-
-    dice_light = Light( object_id="a-dice_light",
-                        position=Position(0, 50, -115),
-                        persist=True )
     arena.add_object(dice_light)
-
-
-    global die1_text
-    die1_text = Text( object_id="a-die1_text",
-                      color=Color(0, 0, 0),
-                      position=Position(0, 50, -125),
-                      scale=Scale(100, 100, 100),
-                      text="i",
-                      persist=True )
     arena.add_object(die1_text)
 
 
